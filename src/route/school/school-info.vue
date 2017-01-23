37c2ed0d578dc275a7e13cc438e19f2c9e8664c6
HuBinAdd <721435146@qq.com>
2017-01-23 09:10:50 +0800

    Revert "Revert "备份脚手架""
    This reverts commit 64d24fe50b474e55e8536657ccc7140584292081.

---
+++ src/route/school/school-info.vue
@@ -0,0 +1,358 @@
+<template>
+<div>
+    <div class="header-title">
+        <a class="weix-logo-index" href="#"><i class="icon-logo-qw iconfont"></i></a>
+        <a class="weix-xuec" href="#">杭州1217学车</a>
+    </div>
+    <div class="main-fiex">
+        <div class="jxxq-header">
+            <a href="#" class="jxxq-header-img">
+                <img src="images/jiaxiao-fec-m1.jpg">
+                <span class="page">共12张</span>
+            </a>
+            <h1 class="jxxq-header-h1">杭州1217学车</h1>
+            <p class="jxxq-header-money">￥4580起</p>
+            <p class="jxx1-header-tip">已有20人通过学车啦报名</p>
+        </div>
+        <ul class="jxxq-renz-lists">
+            <li>
+                <i class="iconfont icon-xq-xuecl"></i> 交警认证
+            </li>
+            <li>
+                <i class="iconfont icon-xq-gongs"></i> 工商认证
+            </li>
+            <li>
+                <i class="iconfont icon-xq-jiaoj"></i> 学车啦认证
+            </li>
+        </ul>
+        <div href="#" class="jxx1-baozj">
+            <i class="iconfont icon-xq-baozj"></i>
+            <span>保障金保障</span>
+            <i class="iconfont icon-xq-xiayg"></i>
+        </div>
+        <h1 class="index-h1">驾校动态</h1>
+        <div class="jxxp-dt-lists">
+            <a class="jxxp-dt-list">
+                <img src="images/banner-n3.jpg">
+            </a>
+            <a class="jxxp-dt-list">
+                <img src="images/banner-n3.jpg">
+            </a>
+            <a class="jxxp-dt-list">
+                <img src="images/banner-n3.jpg">
+            </a>
+            <a class="jxxp-dt-more">
+                <span>查看更多></span>
+            </a>
+        </div>
+        <h1 class="index-h1">在线报名</h1>
+        <ul class="jxxp-baom mt10">
+            <li class="jxxp-baom-list">
+                <span class="ui-radio-n1">
+                        <h1 class="jxxp-baom-h1">
+                            C1普通班
+                            <span class="zqi">培训周期：90天</span>
+                </h1>
+                <p class="jxxp-baom-jiag">
+                    ￥<strong>4580</strong>
+                    <span class="msj">门市价￥4980</span>
+                </p>
+                <div class="jxxp-baom-tip">
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                一人一车
+                            </span>
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                轻松学车
+                            </span>
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                不过包赔
+                            </span>
+                </div>
+                <a class="jxxp-form-btn">报名</a>
+                </span>
+            </li>
+            <li class="jxxp-baom-list">
+                <span class="ui-radio-n1">
+                        <h1 class="jxxp-baom-h1">
+                            C1普通班
+                            <span class="zqi">培训周期：90天</span>
+                </h1>
+                <p class="jxxp-baom-jiag">
+                    ￥<strong>4580</strong>
+                    <span class="msj">门市价￥4980</span>
+                </p>
+                <div class="jxxp-baom-tip">
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                一人一车
+                            </span>
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                轻松学车
+                            </span>
+                    <span>
+                                <i class="iconfont icon-xq-baom"></i>
+                                不过包赔
+                            </span>
+                </div>
+                <a class="jxxp-form-btn">报名</a>
+                </span>
+            </li>
+        </ul>
+        <div class="jxxp-form mt-1" id="jxxp-form1" style="display: none">
+            <div class="jxxp-form-table">
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-name"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="text" name="" placeholder="请输入您的姓名">
+                        </div>
+                    </div>
+                </div>
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-phone"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="number" name="" placeholder="请输入您的联系号码">
+                        </div>
+                    </div>
+                </div>
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-blog"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="text" name="" placeholder="(留言选填)">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="jxxp-form-btn">立即报名</a>
+        </div>
+        <div class="jxxp-position-lists">
+            <a class="jxxp-position-list">
+                <i class="iconfont icon-xq-position"></i> 报名：
+                <span>万塘路东方高新科技园</span>
+                <i class="iconfont icon-xq-xiayg"></i>
+            </a>
+            <a class="jxxp-position-list">
+                <i class="iconfont icon-xq-position"></i> 培训：
+                <span>万塘路东方高新科技园</span>
+                <i class="iconfont icon-xq-xiayg"></i>
+            </a>
+        </div>
+        <h1 class="index-h1">招生范围</h1>
+        <div class="jxxp-zsfw mt10">西湖区、江干区、拱墅区、下城区、萧山区 、余杭区、富阳区</div>
+        <h1 class="index-h1">在线留言</h1>
+        <div class="jxxp-form mt10">
+            <div class="jxxp-form-table">
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-name"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="text" name="" placeholder="请输入您的姓名">
+                        </div>
+                    </div>
+                </div>
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-phone"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="number" name="" placeholder="请输入您的联系号码">
+                        </div>
+                    </div>
+                </div>
+                <div class="jxxp-form-tr">
+                    <span>
+                            <i class="iconfont icon-xq-blog"></i>
+                        </span>
+                    <div class="jxxp-form-td">
+                        <div class="input">
+                            <input type="text" name="" placeholder="(留言选填)">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="jxxp-form-btn">立即报名</a>
+        </div>
+        <h1 class="index-h1">驾校简介</h1>
+        <p class="jxxp-jiax-jj mt10">
+            宁波市海曙金通驾驶培训中心（以下简称金通驾校）成立于2006年1月，旗下拥有包括：电大驾校、捷达驾校、天宁驾校、安达驾校、五大驾校联合-金通驾培连锁机构，训练场面积达6.5万㎡，教练车250余辆，教职员工超30超30超30超300名
+        </p>
+        <span id="lock_more" class="jxxp-lock-more mt-1">查看全部</span>
+        <h1 class="index-h1">
+                驾校评价
+                <a class="timely-assess"><i class="iconfont icon-pingjia-info"></i>立即评价</a>
+            </h1>
+        <div class="jxly-list mt10">
+            <h4 class="jxly-info">
+                    <span class="name">麦子</span>
+                    <span class="date">2016年07月31日</span>
+                    <span class="time">11:42:30</span>
+                </h4>
+            <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+            </p>
+        </div>
+        <div class="jxly-list">
+            <h4 class="jxly-info">
+                    <span class="name">麦子</span>
+                    <span class="date">2016年07月31日</span>
+                    <span class="time">11:42:30</span>
+                </h4>
+            <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+            </p>
+        </div>
+        <div class="jxly-list">
+            <h4 class="jxly-info">
+                    <span class="name">麦子</span>
+                    <span class="date">2016年07月31日</span>
+                    <span class="time">11:42:30</span>
+                </h4>
+            <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+            </p>
+        </div>
+        <div class="jxly-list">
+            <h4 class="jxly-info">
+                    <span class="name">麦子</span>
+                    <span class="date">2016年07月31日</span>
+                    <span class="time">11:42:30</span>
+                </h4>
+            <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+            </p>
+        </div>
+        <div class="jxly-list">
+            <h4 class="jxly-info">
+                    <span class="name">麦子</span>
+                    <span class="date">2016年07月31日</span>
+                    <span class="time">11:42:30</span>
+                </h4>
+            <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+            </p>
+        </div>
+        <div class="jxxp-list-nodata">暂无评价</div>
+        <a class="jxxp-lock-more">查看全部100条评价>></a>
+        <h1 class="index-h1">
+                驾校留言
+                <a class="timely-assess"><i class="iconfont icon-liouyan-info"></i>立即留言</a>
+            </h1>
+        <div class="mt10">
+            <div class="jxly-list">
+                <h4 class="jxly-info">
+        <span class="name">麦子</span>
+        <span class="date">2016年07月31日</span>
+        <span class="time">11:42:30</span>
+    </h4>
+                <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！</p>
+            </div>
+            <div class="jxly-list">
+                <h4 class="jxly-info">
+        <span class="name">麦子</span>
+        <span class="date">2016年07月31日</span>
+        <span class="time">11:42:30</span>
+    </h4>
+                <p class="content">听说是个好驾校，轻松学车没烦恼，我来试学一节课， 教练认真非常好！
+                </p>
+                <div class="jxly-huif">
+                    <div>
+                        <span class="name">驾校回复</span>：您好，请说汉语。或者致电4000410001, 谢谢~我们将竭诚为您服务~
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="jxxp-list-nodata">暂无评价</div>
+        <a class="jxxp-lock-more">查看全部100条留言>></a>
+    </div>
+    <div class="jxxp-footer">
+        <a href="#">
+            <i class="iconfont icon-xq-zx"></i> 在线咨询
+        </a>
+        <a href="#" class="phone">
+            <i class="iconfont icon-xq-dianhua"></i> 电话咨询
+        </a>
+    </div>
+    <div class="layer xq-phone">
+        <div class="layer-xq-phone">
+            <h2>温馨提示</h2>
+            <p>打电话时请告知是在学车啦看到的电话</p>
+            <p>以便享受更优质的服务，谢谢。</p>
+            <div class="tip">40004-10001</div>
+            <div class="close">取消</div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+    },
+    beforeCreate() {},
+    watch: {
+        '$route' (to, from) {
+            console.log(arguments)
+                // 对路由变化作出响应...
+        }
+    },
+    // example code (if you need to get the current swiper object, you can find the swiper object like this, the $ref object is a ref attribute corresponding to the dom redefined)
+    // 如果你需要得到当前的swiper对象来做一些事情，你可以像下面这样定义一个方法属性来获取当前的swiper对象，实际上这里的$refs对应的是当前组件内所有关联了ref属性的组件元素对象，同时配置中的notNextTick属性一定要设置为true
+    computed: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style>
+.main-fiex {
+    padding-top: 1.03rem;
+}
+
+.swiper-slide img {
+    width: 100%;
+    min-height: 100%;
+}
+
+.banner-box .banner-dian {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: .17rem;
+    line-height: .17rem;
+    text-align: center;
+    vertical-align: top;
+}
+
+.banner-box .banner-dian .swiper-pagination-bullet {
+    display: inline-block;
+    width: .1rem;
+    height: .1rem;
+    margin: 0 .1rem;
+    border-radius: .05rem;
+    background: rgba(255, 255, 255, 0.3);
+    vertical-align: top;
+}
+
+.banner-box .banner-dian .swiper-pagination-bullet.swiper-pagination-bullet-active {
+    background: #fff;
+}
+</style>
